c64835e97b3ab02f97b506e3d85a84ea6c68dce8
ZoharRo <zozozohar@gmail.com>
2022-03-03 12:55:53 +0200

    moudle fix

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
-const creds = require('../my.js')
+const creds = require('../my')
+console.log('the creds',creds);
 
 module.exports = {
   'dbURL': `mongodb+srv://${creds}@cluster0.i8g4i.mongodb.net/easylifeDB?retryWrites=true&w=majority`,
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
-const creds = require('../my.js')
+const creds = require('../my')
 
 module.exports = {
   'dbURL': `mongodb+srv://${creds}@cluster0.i8g4i.mongodb.net/easylifeDB?retryWrites=true&w=majority`,
